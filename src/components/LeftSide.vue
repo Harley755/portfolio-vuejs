9a5f4b36ccc6888b9220bf33aaae5ec93e98ca43
Brice GOUDALO <bricegoudalo@gmail.com>
2025-06-16 09:58:02 +0100

    Update LeftSide.vue

--- src/components/LeftSide.vue
+++ src/components/LeftSide.vue
@@ -103,7 +103,7 @@ onMounted(() => {
               <span
                 class="experienceChild nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span
-                class="experienceChild nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Experience</span>
+                class="experienceChild nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Experiences</span>
             </a>
           </li>
           <li>
